9dd605ecc2e11d3c35b23e365b1eddd8791fac79
Jaco Lüken <j.lueken@mhq-services.com>
2020-03-21 17:57:30 +0100

    Implement Login and SignUp page

---
+++ pages/sign-up.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="d-none d-lg-block col-lg-6">
+        <h1>Never home alone again</h1>
+        <p>Lorem ipsum...</p>
+      </div>
+      <div class="col-12 col-lg-6">
+        <h1>Registrierung</h1>
+        <sign-up-form />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignUpForm from '~/components/authentification/SignUpForm.vue'
+
+export default {
+  layout: 'authentication',
+  components: {
+    SignUpForm
+  }
+}
+</script>
